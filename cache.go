8577a544168d1c12fa42f9b5e66e5794a62f354e
Artem Rodimov <artyomrodimov@yandex.ru>
2022-05-26 01:47:54 +0300

    added solution

--- cache.go
+++ cache.go
@@ -3,21 +3,53 @@ package cache
 import "time"
 
 type Cache struct {
+	data      map[string]string
+	deadlines map[string]time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{make(map[string]string),
+		map[string]time.Time{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
+func (c *Cache) Get(key string) (string, bool) {
+	if val, ok := c.data[key]; ok {
+		deadline, isHasDeadline := c.deadlines[key]
+		if isHasDeadline && time.Now().After(deadline) {
+			delete(c.data, key)
+			delete(c.deadlines, key)
+		} else {
+			return val, true
+		}
+	}
 
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	_, isHasDeadline := c.deadlines[key]
+
+	if isHasDeadline {
+		delete(c.deadlines, key)
+	}
+
+	c.data[key] = value
 }
 
-func (receiver) Keys() []string {
+func (c *Cache) Keys() []string {
+	var keys []string
+
+	for key, _ := range c.data {
+		deadline, isHasDeadline := c.deadlines[key]
+		if !isHasDeadline || time.Now().Before(deadline) {
+			keys = append(keys, key)
+		}
+	}
+
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c *Cache) PutTill(key, value string, deadline time.Time) {
+	c.data[key] = value
+	c.deadlines[key] = deadline
 }
